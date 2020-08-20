73a3005ce4933b55c1a59c7d258a4739f75dbbb9
Jayesh Srivastava <jayesh0200@gmail.com>
2020-08-21 02:56:42 +0530

    Create Comments.kt

---
+++ Comments.kt
@@ -0,0 +1,8 @@
+fun main() {
+  //Code prints "Howdy, partner!" to the terminal.
+  println("Howdy, partner!") 
+  /*
+  So far learned about the 'fun' keyword, the 'main()'  keyword,
+  difference between println() and print() and different commenting types.
+  */
+}
