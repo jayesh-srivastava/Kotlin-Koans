39559e7beaf6f526bc5d687bcb62d3ca11caf151
Jayesh Srivastava <jayesh0200@gmail.com>
2020-09-07 23:12:19 +0530

    Update WizardOfOz.kt

--- WizardOfOz.kt
+++ WizardOfOz.kt
@@ -1,1 +1,6 @@
-
+fun main() {
+  val dog = "Toto"
+  val state = "Kansas"
+  val movie = "The Wizard of Oz"
+  println(dog + ", I've a feeling we're not in " + state + " anymore. - " + movie + ", 1939")
+}
